7db6331fbf910c9da96236f3e0b405a1f3f5877b
david <davmwa90@gmail.com>
2022-12-10 09:34:36 +0300

    Completed the Phase-1-algorithms lab

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  // splitting the word using the inbuilt split function 
+  // reversing the word and then joining using the reverse and split functions 
+  const arrayOfLetters = word.split('')
+  const reverseLetters = arrayOfLetters.reverse()
+  const reverseString  = reverseLetters.join('')
 
-/* 
-  Add your pseudocode here
-*/
+
+// Add your pseudocode here
+  if (word == reverseString) {
+    return true 
+  }
+  else {
+    return false 
+  }}
 
 /*
-  Add written explanation of your solution here
+Add written explanation of your solution here
+  if (require.main === module) {
+    console.log('Expecting: false)
+    console.log('=>', isPalindrome('boy'));
+  }
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
@@ -23,3 +39,4 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+
